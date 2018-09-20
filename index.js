d964be4f45ecfa4f53b9fe111665cb53a618637f
haykam821 <24855774+haykam821@users.noreply.github.com>
2018-09-20 13:38:45 -0400

    Load JSON (to default) even if invalid

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ const fs = require("fs-extra");
 
 async function loadJSON(path = "./game.json") {
 	await fs.ensureFile(path);
-	const game = await fs.loadJSON(path);
-	
+	const game = await fs.readJSON(path).catch(() => {
+    		fs.writeJSON(path, {});
+  	});
+
 	return Object.assign({
 		players: [],
 	}, game);
