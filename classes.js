6e7fb7b8d4f58c93aedd56baf435e0121f9521c4
haykam821 <24855774+haykam821@users.noreply.github.com>
2018-09-20 14:45:38 -0400

    Update classes.js

--- classes.js
+++ classes.js
@@ -1,7 +1,15 @@
-class Entity {
+const { EventTarget } = require("yaeti");
+
+class Entity extends EventTarget {
 	constructor(data) {
+		super();
+		
 		this.x = data.x;
 		this.y = data.y;
+		
+		this.direction = 0;
+		
+		this.size = 5;
 	}
 	
 	getForClient() {
@@ -9,7 +17,49 @@ class Entity {
 	}
 }
 
+class PLayer extends Entity {
+	constructor(data) {
+		super(data);
+		
+		this.isSlow = false;
+		
+		this.isSprinting = false;
+		this.boost = 1;
+	}
+	
+	getSpeed() {
+		const speedMultiplier = (this.isSprinting && this.boost) * 1.5;
+		return (this.isSlow ? 2 : 5) * speedMultiplier;
+	}
+
+	launch() {}
+}
+
 module.exports = {
 	object: Entity,
-	trap: Entity,
+	player: Player,
+	spike: class extends Entity {
+		constructor(data) {
+			super(data);
+			
+			this.size = 4;
+			
+			this.addEventListener("collide", event => {
+				if (event.entity instanceof Player) {
+					event.entity.launch(5, event.entity.direction - 180);
+				}
+			});
+		}
+	},
+	trap: class extends Entity {
+		constructor(data) {
+			super(data);
+			
+			this.addEventListener("collide", event => {
+				if (event.entity instanceof Player) {
+					event.entity.isSlow = !!event.status;
+				}
+			});
+		}
+	},
 };
